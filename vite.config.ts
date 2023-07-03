9c722d9261f565e8d6ae8d6f358cf705719fc04f
Naxisigut <834525217@qq.com>
2023-07-03 23:39:28 +0800

    feat: 配置@路径别名

--- vite.config.ts
+++ vite.config.ts
@@ -1,5 +1,6 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import path from 'path'; // 新增
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -14,4 +15,9 @@ export default defineConfig({
       }
     }
   })],
+  resolve:{
+    alias: {
+      '@': path.resolve(__dirname, './src')
+    }
+  }
 })
