6e81a7cead3fdbc1a7f6a457f8b3880f85022e93
Naxisigut <834525217@qq.com>
2023-07-04 01:21:33 +0800

    fix:  碰撞检测逻辑

--- src/game/hit.ts
+++ src/game/hit.ts
@@ -7,7 +7,22 @@ type rectObj = {
   height: number
 }
 
-export const hitCheck = (a, b)=>{
-  if((a.x + a.width) > b.x && (a.y + a.height) > b.y)return true
+export const hitCheck = (a: rectObj, b: rectObj)=>{
+  console.log( 1, a.x + a.width);
+  console.log( 2, b.x);
+  console.log( 3, a.y + a.height);
+  console.log( 4, b.y);
+
+  // const centerXA = (a.x + a.width) / 2
+  // const centerYA = (a.y + a.height) / 2
+  // const centerXB = (a.x + a.width) / 2
+  // const centerYA = (a.y + a.height) / 2
+
+  const xCoincide = (a.x < b.x + b.width) && (a.x + a.width > b.x)
+  const yCoincide = (a.y < b.y + b.height) && (a.y + a.height > b.y)
+  if(xCoincide && yCoincide) return true
   return false
+
+  // if((a.x + a.width) > b.x && (a.y + a.height) > b.y)return true
+  // return false
 }
